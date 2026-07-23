8df989726711862d06e76eeb0da378c0c8550016
Marlin <wowisdone@gmail.com>
2026-07-23 13:30:18 -0400

    feat(repairs): add save repair action

--- src/features/repairs/pages/CreateRepairPage.tsx
+++ src/features/repairs/pages/CreateRepairPage.tsx
@@ -5,17 +5,32 @@ import OperationsSection from "../operations/components/OperationsSection";
 import NotesSection from "../notes/components/NotesSection";
 import AttachmentsSection from "../attachments/components/AttachmentsSection";
 
-
 export default function CreateRepairPage() {
+
+    function handleSaveRepair() {
+        console.log("Save Repair");
+    }
+
     return (
         <main className="create-repair-page">
+
             <RepairHeader />
 
             <OperationsSection />
 
             <NotesSection />
-            
+
             <AttachmentsSection />
+
+            <div className="repair-actions">
+                <button
+                    className="save-repair-button"
+                    onClick={handleSaveRepair}
+                >
+                    Save Repair
+                </button>
+            </div>
+
         </main>
     );
 }
